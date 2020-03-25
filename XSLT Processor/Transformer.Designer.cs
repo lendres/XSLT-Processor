6d955b613d61eefa0e747c058392225c1c6f5af4
Lance A. Endres <lendres@fifthrace.com>
2020-03-25 15:45:44 -0500

    Updated tab order of control.

--- XSLT Processor/Transformer.Designer.cs
+++ XSLT Processor/Transformer.Designer.cs
@@ -62,7 +62,7 @@ namespace DigitalProduction.XSTProcessor
 			this.buttonProcess.Location = new System.Drawing.Point(413, 273);
 			this.buttonProcess.Name = "buttonProcess";
 			this.buttonProcess.Size = new System.Drawing.Size(75, 23);
-			this.buttonProcess.TabIndex = 0;
+			this.buttonProcess.TabIndex = 2;
 			this.buttonProcess.Text = "&Process";
 			this.buttonProcess.UseVisualStyleBackColor = true;
 			this.buttonProcess.Click += new System.EventHandler(this.buttonProcess_Click);
@@ -75,7 +75,7 @@ namespace DigitalProduction.XSTProcessor
 			this.buttonClose.Location = new System.Drawing.Point(494, 273);
 			this.buttonClose.Name = "buttonClose";
 			this.buttonClose.Size = new System.Drawing.Size(75, 23);
-			this.buttonClose.TabIndex = 1;
+			this.buttonClose.TabIndex = 3;
 			this.buttonClose.Text = "&Close";
 			this.buttonClose.UseVisualStyleBackColor = true;
 			this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
@@ -87,7 +87,7 @@ namespace DigitalProduction.XSTProcessor
 			this.textBoxInputFile.Location = new System.Drawing.Point(12, 36);
 			this.textBoxInputFile.Name = "textBoxInputFile";
 			this.textBoxInputFile.Size = new System.Drawing.Size(449, 20);
-			this.textBoxInputFile.TabIndex = 14;
+			this.textBoxInputFile.TabIndex = 1;
 			// 
 			// buttonBrowseInputFile
 			// 
@@ -95,7 +95,7 @@ namespace DigitalProduction.XSTProcessor
 			this.buttonBrowseInputFile.Location = new System.Drawing.Point(467, 34);
 			this.buttonBrowseInputFile.Name = "buttonBrowseInputFile";
 			this.buttonBrowseInputFile.Size = new System.Drawing.Size(80, 23);
-			this.buttonBrowseInputFile.TabIndex = 13;
+			this.buttonBrowseInputFile.TabIndex = 2;
 			this.buttonBrowseInputFile.Text = "Browse...";
 			this.buttonBrowseInputFile.UseVisualStyleBackColor = true;
 			this.buttonBrowseInputFile.Click += new System.EventHandler(this.buttonBrowseInputFile_Click);
@@ -106,7 +106,7 @@ namespace DigitalProduction.XSTProcessor
 			this.labelInputFile.Location = new System.Drawing.Point(12, 20);
 			this.labelInputFile.Name = "labelInputFile";
 			this.labelInputFile.Size = new System.Drawing.Size(75, 13);
-			this.labelInputFile.TabIndex = 12;
+			this.labelInputFile.TabIndex = 0;
 			this.labelInputFile.Text = "XML Input File";
 			// 
 			// textBoxXsltFile
@@ -116,7 +116,7 @@ namespace DigitalProduction.XSTProcessor
 			this.textBoxXsltFile.Location = new System.Drawing.Point(12, 83);
 			this.textBoxXsltFile.Name = "textBoxXsltFile";
 			this.textBoxXsltFile.Size = new System.Drawing.Size(449, 20);
-			this.textBoxXsltFile.TabIndex = 17;
+			this.textBoxXsltFile.TabIndex = 4;
 			// 
 			// buttonBrowseXsltFile
 			// 
@@ -124,7 +124,7 @@ namespace DigitalProduction.XSTProcessor
 			this.buttonBrowseXsltFile.Location = new System.Drawing.Point(467, 81);
 			this.buttonBrowseXsltFile.Name = "buttonBrowseXsltFile";
 			this.buttonBrowseXsltFile.Size = new System.Drawing.Size(80, 23);
-			this.buttonBrowseXsltFile.TabIndex = 16;
+			this.buttonBrowseXsltFile.TabIndex = 5;
 			this.buttonBrowseXsltFile.Text = "Browse...";
 			this.buttonBrowseXsltFile.UseVisualStyleBackColor = true;
 			this.buttonBrowseXsltFile.Click += new System.EventHandler(this.buttonBrowseXsltFile_Click);
@@ -135,7 +135,7 @@ namespace DigitalProduction.XSTProcessor
 			this.labelXsltFile.Location = new System.Drawing.Point(12, 67);
 			this.labelXsltFile.Name = "labelXsltFile";
 			this.labelXsltFile.Size = new System.Drawing.Size(53, 13);
-			this.labelXsltFile.TabIndex = 15;
+			this.labelXsltFile.TabIndex = 3;
 			this.labelXsltFile.Text = "XSLT File";
 			// 
 			// textBoxOutputFile
@@ -145,7 +145,7 @@ namespace DigitalProduction.XSTProcessor
 			this.textBoxOutputFile.Location = new System.Drawing.Point(12, 128);
 			this.textBoxOutputFile.Name = "textBoxOutputFile";
 			this.textBoxOutputFile.Size = new System.Drawing.Size(449, 20);
-			this.textBoxOutputFile.TabIndex = 20;
+			this.textBoxOutputFile.TabIndex = 7;
 			// 
 			// buttonBrowseOutputFile
 			// 
@@ -153,7 +153,7 @@ namespace DigitalProduction.XSTProcessor
 			this.buttonBrowseOutputFile.Location = new System.Drawing.Point(467, 126);
 			this.buttonBrowseOutputFile.Name = "buttonBrowseOutputFile";
 			this.buttonBrowseOutputFile.Size = new System.Drawing.Size(80, 23);
-			this.buttonBrowseOutputFile.TabIndex = 19;
+			this.buttonBrowseOutputFile.TabIndex = 8;
 			this.buttonBrowseOutputFile.Text = "Browse...";
 			this.buttonBrowseOutputFile.UseVisualStyleBackColor = true;
 			this.buttonBrowseOutputFile.Click += new System.EventHandler(this.buttonBrowseOutputFile_Click);
@@ -164,7 +164,7 @@ namespace DigitalProduction.XSTProcessor
 			this.labelOutputFile.Location = new System.Drawing.Point(12, 112);
 			this.labelOutputFile.Name = "labelOutputFile";
 			this.labelOutputFile.Size = new System.Drawing.Size(58, 13);
-			this.labelOutputFile.TabIndex = 18;
+			this.labelOutputFile.TabIndex = 6;
 			this.labelOutputFile.Text = "Output File";
 			// 
 			// textBoxPostProcessor
@@ -174,7 +174,7 @@ namespace DigitalProduction.XSTProcessor
 			this.textBoxPostProcessor.Location = new System.Drawing.Point(12, 39);
 			this.textBoxPostProcessor.Name = "textBoxPostProcessor";
 			this.textBoxPostProcessor.Size = new System.Drawing.Size(449, 20);
-			this.textBoxPostProcessor.TabIndex = 22;
+			this.textBoxPostProcessor.TabIndex = 1;
 			// 
 			// buttonBrowsePostProcessor
 			// 
@@ -182,7 +182,7 @@ namespace DigitalProduction.XSTProcessor
 			this.buttonBrowsePostProcessor.Location = new System.Drawing.Point(467, 37);
 			this.buttonBrowsePostProcessor.Name = "buttonBrowsePostProcessor";
 			this.buttonBrowsePostProcessor.Size = new System.Drawing.Size(80, 23);
-			this.buttonBrowsePostProcessor.TabIndex = 21;
+			this.buttonBrowsePostProcessor.TabIndex = 2;
 			this.buttonBrowsePostProcessor.Text = "Browse...";
 			this.buttonBrowsePostProcessor.UseVisualStyleBackColor = true;
 			// 
@@ -192,7 +192,7 @@ namespace DigitalProduction.XSTProcessor
 			this.checkBoxPostProcessor.Location = new System.Drawing.Point(12, 20);
 			this.checkBoxPostProcessor.Name = "checkBoxPostProcessor";
 			this.checkBoxPostProcessor.Size = new System.Drawing.Size(120, 17);
-			this.checkBoxPostProcessor.TabIndex = 23;
+			this.checkBoxPostProcessor.TabIndex = 0;
 			this.checkBoxPostProcessor.Text = "Run Post Processor";
 			this.checkBoxPostProcessor.UseVisualStyleBackColor = true;
 			this.checkBoxPostProcessor.CheckedChanged += new System.EventHandler(this.checkBoxPostProcessor_CheckedChanged);
@@ -211,7 +211,7 @@ namespace DigitalProduction.XSTProcessor
 			this.groupBoxProcessor.Location = new System.Drawing.Point(10, 8);
 			this.groupBoxProcessor.Name = "groupBoxProcessor";
 			this.groupBoxProcessor.Size = new System.Drawing.Size(559, 162);
-			this.groupBoxProcessor.TabIndex = 24;
+			this.groupBoxProcessor.TabIndex = 0;
 			this.groupBoxProcessor.TabStop = false;
 			this.groupBoxProcessor.Text = "XSLT Processing";
 			// 
@@ -223,7 +223,7 @@ namespace DigitalProduction.XSTProcessor
 			this.groupBoxPostProcessing.Location = new System.Drawing.Point(10, 183);
 			this.groupBoxPostProcessing.Name = "groupBoxPostProcessing";
 			this.groupBoxPostProcessing.Size = new System.Drawing.Size(559, 74);
-			this.groupBoxPostProcessing.TabIndex = 25;
+			this.groupBoxPostProcessing.TabIndex = 1;
 			this.groupBoxPostProcessing.TabStop = false;
 			this.groupBoxPostProcessing.Text = "Post Processing";
 			// 
